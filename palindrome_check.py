fb06879ae2ea9321ed0eeff361e8b4379b54d5e6
Rokon Ron <rokon.raz@gmail.com>
2023-09-10 23:28:33 +0600

    only simple find palindrome number.

---
+++ palindrome_check.py
@@ -0,0 +1,12 @@
+
+
+for x in range(10,300):
+    z = str(x)
+    y = z[::-1]
+    if z == y:
+        print(f"its palindrome {x}")
+
+    else:
+        pass
+        # print(f"its not palindrome {x}")
+
