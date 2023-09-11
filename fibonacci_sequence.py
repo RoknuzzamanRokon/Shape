b6616793abcb60f5365d9a5b9a0da3f5bce2ea42
Rokon Ron <rokon.raz@gmail.com>
2023-09-11 12:29:58 +0600

    Here calculate the fibonacci sequence using function.

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -8,3 +8,22 @@ while count < 10:
     num_1 = num_2
     num_2 = num_3
     count += 1
+
+
+# Here using only function.
+def fibonacci(value):
+    count_1 = 0
+    num_f_1 = 0
+    num_f_2 = 1
+    while count_1 < value:
+        print(num_f_1)
+        num_f_3 = num_f_1 + num_f_2
+        num_f_1 = num_f_2
+        num_f_2 = num_f_3
+        count_1 += 1
+
+fibonacci(10)
+
+
+
+
